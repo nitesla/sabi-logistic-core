a2d0e33bddbbd6df90711ab0407fc53b4bc281d1
Davyjones <ekenedavidson15@gmail.com>
2021-10-02 04:51:40 +0100

    new file

---
+++ src/main/java/com/sabilogisticscore/SabiLogisticCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sabilogisticscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabiLogisticCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabiLogisticCoreApplication.class, args);
+	}
+
+}
