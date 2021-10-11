385d8285f473da6ee5387e1331cf226139912283
Davyjones <ekenedavidson15@gmail.com>
2021-10-11 18:43:44 +0100

    category

---
+++ src/main/java/com/sabilogisticscore/models/Category.java
@@ -0,0 +1,21 @@
+package com.sabilogisticscore.models;
+
+
+import com.sabi.framework.models.CoreEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper=false)
+@Data
+@Entity
+public class Category extends CoreEntity {
+
+    private String name;
+    private String image;
+    private String description;
+    private boolean hasAsset;
+    private boolean hasWareHouse;
+
+}
