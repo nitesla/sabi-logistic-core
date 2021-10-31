f6b046002c0f653ed3a1e0470051960b3566e610
Davyjones <ekenedavidson15@gmail.com>
2021-10-31 15:51:59 +0100

    new update

--- src/main/java/com/sabi/logistics/core/dto/response/PartnerAssetResponseDto.java
+++ src/main/java/com/sabi/logistics/core/dto/response/PartnerAssetResponseDto.java
@@ -5,10 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
