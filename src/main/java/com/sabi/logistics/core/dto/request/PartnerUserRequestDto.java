eed7d3120f154dcbd087e1de066e0bd250b11e2f
Davyjones <ekenedavidson15@gmail.com>
2021-11-09 09:04:42 +0100

    new

--- src/main/java/com/sabi/logistics/core/dto/request/PartnerUserRequestDto.java
+++ src/main/java/com/sabi/logistics/core/dto/request/PartnerUserRequestDto.java
@@ -5,18 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class PartnerUserRequestDto {
+
     private Long id;
-    @NotNull(message = "partnerId can not be null")
-    @Min(value = 1, message = "Partner Id can not be less than zero")
-    private Long partnerId;
-    @NotNull(message = "User Id can not be null")
-    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String phone;
+
+
 }
