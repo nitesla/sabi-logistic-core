20ecfdda1fce016dc5b3ab06798a5130be2dfe1a
Davyjones <ekenedavidson15@gmail.com>
2021-12-07 08:36:53 +0100

    new

--- src/main/java/com/sabi/logistics/core/models/Bank.java
+++ src/main/java/com/sabi/logistics/core/models/Bank.java
@@ -3,9 +3,11 @@ package com.sabi.logistics.core.models;
 import com.sabi.framework.models.CoreEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper=false)
 @Data
 @Entity
