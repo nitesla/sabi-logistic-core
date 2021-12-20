2230d61603e45449272ab5aa0db04621ad36288a
muyiwa-spinel <muyiwa.a@spinel.consulting>
2021-12-20 01:47:37 -0800

    update++

---
+++ src/main/java/com/sabi/logistics/core/dto/response/TripRequestStatusCountResponse.java
@@ -0,0 +1,19 @@
+package com.sabi.logistics.core.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TripRequestStatusCountResponse {
+
+    private int pending;
+    private int accepted;
+    private  int rejected;
+}
