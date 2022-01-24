1dabfeef83b283d61c80bbe5a75eda254187ce27
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2022-01-25 00:45:23 +0100

    +++ update

---
+++ src/main/java/com/sabi/logistics/core/dto/response/DropOffItemDetailDto.java
@@ -0,0 +1,41 @@
+package com.sabi.logistics.core.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DropOffItemDetailDto {
+    private Long id;
+    private Long dropOffId;
+    private BigDecimal amountCollected;
+    private Integer qtyGoodsReturned;
+    private Integer qtyGoodsDelivered;
+    private String reasonForReturn;
+    private Long thirdPartyProductId;
+    private String productName;
+    private String status;
+    private LocalDateTime deliveryDate;
+    private Long orderItemId;
+    private String orderItemName;
+    private Integer qty;
+    private String customerName;
+    private String customerPhone;
+    private Long orderId;
+    private Long driverId;
+    private String driverName;
+    private Long driverAssistantId;
+    private String driverAssistantName;
+    private String driverAssistantPhone;
+    private String driverPhone;
+
+}
